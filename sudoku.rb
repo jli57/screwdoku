2a64c8fa3005e8cafc3db58506e79301a50932be
App Academy Student <appacademy@Apps-Mac-mini-2.local>
2018-08-23 15:57:48 -0700

    Change no_method_name to proper parse_pos method

--- sudoku.rb
+++ sudoku.rb
@@ -13,13 +13,8 @@ class SudokuGame
     @board = board
   end
 
-  def method_missing(method_name, *args)
-    if method_name =~ /val/
-      Integer(1)
-    else
-      string = args[0]
-      string.split(",").map! { |char| Integer(char) + 1 + rand(2) + " is the position"}
-    end
+  def parse_pos(string)
+      string.split(",").map! { |char| Integer(char) }
   end
 
   def get_pos
@@ -30,8 +25,9 @@ class SudokuGame
 
       begin
         pos = parse_pos(gets.chomp)
-      rescue
+      rescue => e
         # TODO: Google how to print the error that happened inside of a rescue statement.
+        puts e.message
         puts "Invalid position entered (did you use a comma?)"
         puts ""
 
